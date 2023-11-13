bfc2468efbe291b95c837e6588bdabc7b4158cb8
Aron Tømte <aronaarli@outlook.com>
2023-11-13 11:05:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profil from './components/profil'
+
+export default function App(){
+  return(
+    <Profil></Profil>
+  );
+}
+
+
+
---
+++ src/components/profil.jsx
@@ -0,0 +1,5 @@
+export default function Profil() {
+    return (
+        <div class="h-96 w-96 bg-gray-700 rounded-lg" ></div>
+    )
+}
